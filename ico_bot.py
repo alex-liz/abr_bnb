5113871fa9968bdbfbb148a8dead0a3be10106df
alex-liz <alejandrolizaguilera@gmail.com>
2021-11-13 00:40:23 +0100

    init ico

--- ico_bot.py
+++ ico_bot.py
@@ -22,19 +22,22 @@ async def main():
     usdt_qty = 100
     client = AsyncClient(api_key=os.environ['API_KEY'], api_secret=os.environ['API_SECRET'])
     crypto_con = try_connection(crypto_symbol=crypto_symbol, client=client)
-    async with crypto_con.get_bnbsm() as tscm_crypto_usdt:
-        res = await tscm_crypto_usdt.recv()
-        dt_crypto_usdt = create_dataframe_get(res)
-        quantity_trade = usdt_qty / dt_crypto_usdt['Price'].iloc[-1]
-        print(f"Quantity to buy {quantity_trade}")
-        print(f"Buy order at: \n {dt_crypto_usdt}")
-        # await crypto_con.buy_order_market(qty=quantity_trade)
-    sleep(2)
-    async with crypto_con.get_bnbsm() as tscm_crypto_usdt:
-        res = await tscm_crypto_usdt.recv()
-        dt_crypto_usdt = create_dataframe_get(res)
-        print(f"Sell order at: \n {dt_crypto_usdt}")
-        # await crypto_con.sell_order_market(qty=quantity_trade)
+    while True:
+        async with crypto_con.get_bnbsm() as tscm_crypto_usdt:
+            res = await tscm_crypto_usdt.recv()
+            dt_crypto_usdt = create_dataframe_get(res)
+            quantity_trade = usdt_qty / dt_crypto_usdt['Price'].iloc[-1]
+            buy_price_trade = dt_crypto_usdt['Price'].iloc[-1]
+            print(f"Quantity to buy {quantity_trade}")
+            print(f"Buy order at: \n {dt_crypto_usdt}")
+            # await crypto_con.buy_order_market(qty=quantity_trade)
+        sleep(5)
+        async with crypto_con.get_bnbsm() as tscm_crypto_usdt:
+            res = await tscm_crypto_usdt.recv()
+            dt_crypto_usdt = create_dataframe_get(res)
+            print(f"Sell order at: \n {dt_crypto_usdt}")
+            # await crypto_con.sell_order_market(qty=quantity_trade)
+            print(f"Profit: {dt_crypto_usdt['Price'].iloc[-1] - buy_price_trade}")
     await client.close_connection()
 
 
